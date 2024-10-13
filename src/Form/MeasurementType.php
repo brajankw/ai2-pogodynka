10843f6ecfc5170aae75925a1157bb6bf9a4fe3a
brajankw <brajan.kwiatkowski@protonmail.com>
2024-10-13 19:55:05 +0200

    lab-d

---
+++ src/Form/MeasurementType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use App\Entity\Measurement;
+use phpDocumentor\Reflection\Types\Float_;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('measure_date')
+            ->add('create_date')
+            ->add('celsius',null, [
+                'attr' => [
+                    'placeholder' => 'Enter number of celsius',
+                ]
+            ])
+            ->add('location', EntityType::class, [
+                'class' => Location::class,
+'choice_label' => 'city',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
